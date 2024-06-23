dc77f1ca88350152769c5474cf2596bc525b8fdc
Stephan <stephan.stapel@web.de>
2024-06-23 22:14:37 +0200

    revert change

--- BricklinkSharp.Client/UpdateInventory.cs
+++ BricklinkSharp.Client/UpdateInventory.cs
@@ -40,7 +40,7 @@ public class UpdateInventory
     public decimal? UnitPrice { get; set; }
 
     [JsonPropertyName("description")]
-    public string Description { get; set; } = String.Empty;
+    public string? Description { get; set; }
 
     [JsonPropertyName("remarks")]
     public string? Remarks { get; set; }
